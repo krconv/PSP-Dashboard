c36e93937cc01d6a0469a1e86f958706839b8017
Kodey Converse <kodey@krconv.com>
2017-11-11 19:49:13 -0500

    Update nav links to include active class
    Use NavLinks which have the `active` class when it references the current page, and fixes an error with the app className attribute

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  NavLink
 } from 'react-router-dom';
 
 import Home from './Home';
@@ -12,18 +12,20 @@ import Families from './Families';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div class="App">
-          <nav>
-            <Link exact to="/">Home</Link>
-            <Link exact to="/families">Families</Link>
-          </nav>
-          <main>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/families" component={Families} />
-          </main>
-        </div>
-      </Router>
+      <div className="App">
+        <Router>
+          <div>
+            <nav>
+              <NavLink exact to="/" activeClassName="active">Home</NavLink>
+              <NavLink exact to="/families" activeClassName="active">Families</NavLink>
+            </nav>
+            <main>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/families" component={Families} />
+            </main>
+          </div>
+        </Router>
+      </div>
     );
   }
 }
